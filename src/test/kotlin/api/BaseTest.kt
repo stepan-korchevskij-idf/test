174a6a76215e68d3090d7e7f95b49d6c7b63f4b6
stepan-korchevskij <stepan.korchevskij@moneyman.ru>
2020-03-23 18:47:12 +0300

    minor update

--- src/test/kotlin/api/BaseTest.kt
+++ src/test/kotlin/api/BaseTest.kt
@@ -2,16 +2,18 @@ package api
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
+import config.environment.EnvironmentConfigurationHolder
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 
-class BaseTest {
+open class BaseTest {
   private lateinit var wireMockServer: WireMockServer
+  val envConfig = EnvironmentConfigurationHolder.configuration
   val host = "localhost"
   val port: Int = 8080
 
   @BeforeAll
-  fun createStub() {
+  fun startWireMockServer() {
     wireMockServer = WireMockServer()
     wireMockServer.start()
     WireMock.configureFor(host, port)
--- src/test/kotlin/api/CrmLoginTest.kt
+++ src/test/kotlin/api/CrmLoginTest.kt
@@ -5,9 +5,7 @@ import api.client.data.ContentType
 import api.client.data.CookieType
 import api.client.data.HeaderType
 import api.generator.CrmRequestGenerator
-import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.*
-import config.environment.EnvironmentConfigurationHolder
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
@@ -17,25 +15,14 @@ import org.junit.jupiter.api.function.Executable
 import utils.transformDataToJson
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class CrmLoginTest {
-  private lateinit var wireMockServer: WireMockServer
-  private val envConfig = EnvironmentConfigurationHolder.configuration
-
-  private val host = "localhost"
-  private val port: Int = 8080
+class CrmLoginTest : BaseTest() {
   private val endpoint: String = envConfig.crmSingInEndpoint!!
-
   private val cookie = "JSESSIONID=testSessionId"
-  private val requestBody = "{\"login\":\"admin\",\"password\":\"11111111\"," +
-      "\"captcha\":\"11111\"}"
-  private val responseBody =
-    "{\"id\":1,\"userName\":\"admin\",\"localizedRole\":\"Administrator\",\"roleId\":10,\"error\":null}"
+  private val requestBody = "{\"login\":\"admin\",\"password\":\"11111111\",\"captcha\":\"11111\"}"
+  private val authorizedUser = User(1, "admin", "Administrator", 10, null)
 
   @BeforeAll
   fun createStub() {
-    wireMockServer = WireMockServer()
-    wireMockServer.start()
-    configureFor(host, port)
     stubFor(
       post(urlEqualTo(endpoint))
         .withRequestBody(equalToJson(requestBody))
@@ -45,7 +32,7 @@ class CrmLoginTest {
           aResponse()
             .withHeader("Content-Type", ContentType.APP_JSON.text)
             .withHeader("Set-Cookie", cookie)
-            .withBody(responseBody)
+            .withBody(transformDataToJson(authorizedUser))
         )
     )
   }
@@ -53,7 +40,7 @@ class CrmLoginTest {
   @Test
   fun checkLogInStub() {
     val codeExpected = 200
-    val bodyExpected = transformDataToJson(User(1, "admin", "Administrator", 10, null))
+    val bodyExpected = transformDataToJson(authorizedUser)
     val request = CrmRequestGenerator().getAuthorizeCrmRequest()
     request.url = "http://$host:$port$endpoint"
     val response = CustomClient.sendRequest(request)
