f44738285e788a86018b0b6ef2ad9f3ea726f980
stepan-korchevskij <stepan.korchevskij@moneyman.ru>
2020-03-24 10:16:44 +0300

    minor fix

--- src/test/kotlin/wiremock/Stabs.kt
+++ src/test/kotlin/wiremock/Stabs.kt
@@ -2,6 +2,7 @@ package wiremock
 
 import api.client.data.ContentType
 import api.client.data.HeaderType
+import api.data.AuthorizeForm
 import api.data.AuthorizedUser
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import config.environment.EnvironmentConfigurationHolder
@@ -9,13 +10,13 @@ import utils.transformDataToJson
 
 fun addCrmLoginStub() {
   val envConfig = EnvironmentConfigurationHolder.configuration
-  val requestBody = "{\"login\":\"admin\",\"password\":\"11111111\",\"captcha\":\"11111\"}"
   val cookie = "JSESSIONID=testSessionId"
+  val requestBody = AuthorizeForm("admin", "11111111", "11111")
   val authorizedUser = AuthorizedUser(1, "admin", "Administrator", 10, null)
 
   stubFor(
     post(urlEqualTo(envConfig.crmSingInEndpoint!!))
-      .withRequestBody(equalToJson(requestBody))
+      .withRequestBody(equalToJson(transformDataToJson(requestBody)))
       .withBasicAuth(envConfig.user, envConfig.pass)
       .withHeader(HeaderType.CONTENT_TYPE.text, equalToIgnoreCase(ContentType.APP_JSON.text))
       .willReturn(
